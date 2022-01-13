4a5f6fd14fdb364cc71dbaca431cbf25a23c982d
Palash Bauri <palashbauri1@gmail.com>
2022-01-13 20:05:05 +0530

    Prototyping argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
-use gemjam::jam::builder::structure::StructureBuilder;
+//use gemjam::jam::builder::structure::StructureBuilder;
 //use gemjam::jam::utils::conf_reader::Config;
-use std::collections::HashMap;
-use std::path::PathBuf;
+//use std::collections::HashMap;
+//use std::path::PathBuf;
 //use yaml_rust::Yaml;
-//use std::os;
-
+//use std::env;
+use clap::{Arg,App};
 fn main() {
-    // )
-    let hm: HashMap<String, String> = HashMap::new();
-    let mut s = StructureBuilder::config(
-        hm,
-        PathBuf::from(r"/home/palash/gemjam/myjam/polu/"),
-        PathBuf::from(r"/home/palash/gemjam/myjam/log/"),
-        PathBuf::from(r"/home/palash/gemjam/myjam/polu/templates/"),
-    );
+    
+    let matches = App::new("GemJam")
+                .version("0.1")
+                .author("Palash Bauri <palashbauri1@gmail.com>")
+                .about("simple and kinda fast gemlog maker!")
+                .arg(Arg::new("config")
+                     .short('c')
+                     .long("conf")
+                     .takes_value(true)
+                     .required(false)
+                     .help("Directory where jam.yaml file can be found")).get_matches();
+    println!("config file => {}" , matches.value_of("config").unwrap_or("t"));
 
-    s.render_posts();
+
+
+
+    //println!("{:?}" , env::current_dir().unwrap());
+
+    //let hm: HashMap<String, String> = HashMap::new();
+    //let mut s = StructureBuilder::config(
+    //    hm,
+    //    PathBuf::from(r"/home/palash/gemjam/myjam/polu/"),
+    //    PathBuf::from(r"/home/palash/gemjam/myjam/log/"),
+    //    PathBuf::from(r"/home/palash/gemjam/myjam/polu/templates/"),
+    //);
+
+    //s.render_posts();
 }
