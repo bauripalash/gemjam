4662e19dc68c4ebb5b3d60ddebf0254afb322369
Palash Bauri <palashbauri1@gmail.com>
2022-02-01 10:51:37 +0530

    Fixed deleting output directory

--- src/jam/builder/structure.rs
+++ src/jam/builder/structure.rs
@@ -2,6 +2,8 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 use std::path::*;
 // index.gmi -> index page >> where the post indexting will happen
 // header.gmi -> will be pasted at the top of the index.gmi file
@@ -9,6 +11,15 @@ use std::path::*;
 // vivekananda vs kal marx -> mitri koutillo
 // vivekananda er biplab bad
 //
+
+lazy_static!{
+
+    static ref REX : Regex = Regex::new(r"\[\[\[(?P<reptag>.+)\]\]\]").unwrap();
+    static ref HEADER_REGEX: Regex =
+                Regex::new(r"(?U)(?:\#{1,3}\s)(?P<title>.*)(?:\n)").unwrap();
+
+    static ref POST_REX : Regex = Regex::new(r"\[\[\[(_content_)\]\]\]").unwrap();
+}
 
 #[derive(Debug)]
 pub struct GemPost {
@@ -21,6 +32,9 @@ pub struct GemPost {
 pub struct StructureBuilder {
     pub template_dir: PathBuf,
     pub content_dir: PathBuf,
+    pub output_dir: PathBuf,
+    pub index_entry: PathBuf,
+    pub post_entry_fstring : String,
     pub slug: String,
     pub reptags: HashMap<String, String>,
 }
@@ -29,12 +43,28 @@ impl StructureBuilder {
     pub fn config(
         content_dir: PathBuf,
         template_dir: PathBuf,
+        output_dir : PathBuf,
+        index_entry : PathBuf,
+        post_entry: PathBuf,
         slug: String,
         reptags: HashMap<String, String>,
     ) -> StructureBuilder {
+        if !output_dir.exists(){
+            
+            fs::create_dir(&output_dir).unwrap();
+
+        }else {
+            //fs::remove_dir(&output_dir).unwrap();
+            fs::remove_dir_all(&output_dir).unwrap();
+            fs::create_dir(&output_dir).unwrap();
+            fs::create_dir(&output_dir.join(&slug)).unwrap();
+        }
         Self {
             template_dir,
             content_dir,
+            output_dir,
+            index_entry,
+            post_entry_fstring : fs::read_to_string(post_entry).unwrap(),
             slug,
             reptags,
         }
@@ -58,16 +88,26 @@ impl StructureBuilder {
         output
     }
 
-    pub fn get_post_title(&mut self, p: PathBuf) -> Option<String> {
+    pub fn get_post_title(&mut self, p: PathBuf , file_name : &String) -> Option<String> {
         let mut output: Option<String> = None;
-        lazy_static! {
-            static ref HEADER_REGEX: Regex =
-                Regex::new(r"(?U)(?:\#{1,3}\s)(?P<title>.*)(?:\n)").unwrap();
-        }
+
+        
+           
         let raw_file = fs::read_to_string(p).unwrap();
+        
+        let _out_post = POST_REX.replace(&self.post_entry_fstring, &raw_file);
+        
         for ix in HEADER_REGEX.captures(&raw_file) {
             output = ix.get(1).map_or(None, |s| Some(s.as_str().to_string()));
         }
+        let output_path  = self.output_dir.join(&self.slug).join(file_name.to_owned() + &".gmi".to_string());
+        //if output_path.exists(){
+            
+        //    std::fs::remove_file(&output_path).expect("An outdated file exists in your output directory, and sadly I failed to delete it. So.... bye!");
+
+        //}
+        let mut fdsc = File::create(&output_path).expect("Failed to create a file for your post in output directory!");
+        fdsc.write(_out_post.as_bytes()).expect("Failed to save post!");        //fs::write(output_path, &*_out_post).unwrap();
         output 
     }
 
@@ -92,7 +132,7 @@ impl StructureBuilder {
                 .collect();
             gemposts.push(GemPost {
                 path: _posts.as_ref().unwrap().path(),
-                title: self.get_post_title(_posts.as_ref().unwrap().path().to_path_buf()),
+                title: self.get_post_title(_posts.as_ref().unwrap().path().to_path_buf() , &_fn_vec[0]),
                 slug: _fn_vec[0].clone(),
                 date: _fn_vec[1].clone(),
             });
@@ -101,57 +141,53 @@ impl StructureBuilder {
     }
 
     pub fn render_posts(&mut self) -> bool {
-        let rex = Regex::new(r"\[\[\[(?P<reptag>.+)\]\]\]").unwrap();
+        //let rex = Regex::new(r"\[\[\[(?P<reptag>.+)\]\]\]").unwrap();
         let mut no_error = true;
-        let index_path = &self.template_dir.join("index.gmi");
+        let index_path = &self.index_entry; //&self.template_dir.join("index.gmi");
         let posts_reptag: String = self.reptags.get("_posts_").unwrap().to_string();
-        //let posts_reptag : String = self
-        //println!("{}" , posts_reptag);
         if index_path.exists() {
             let index_raw = fs::read_to_string(index_path).unwrap();
             let mut _raw = index_raw.clone();
-            let x = rex.captures_iter(&index_raw);
+            let x = REX.captures_iter(&index_raw);
             for i in x {
                 let _r = i["reptag"].to_string();
-                //println!("{}" , _r == posts_reptag);
                 if self.reptags.contains_key(&_r) {
                     if _r == posts_reptag {
                         _raw =
                             _raw.replace(i.get(0).unwrap().as_str(), &self.get_post_list_markup());
+
                         continue;
                     }
-
-                    let _replace_with =
-                        fs::read_to_string(self.template_dir.join(self.reptags.get(&_r).unwrap()))
+                    let _rrv = self.reptags.get(&_r).unwrap();
+                    let mut _replace_with = String::new();
+                    if _rrv.starts_with("file=>"){
+                    let _filename = _rrv.strip_prefix("file=>").unwrap();
+                    println!("{}" , _filename);
+                    _replace_with =
+                        fs::read_to_string(self.template_dir.join(_filename))
                             .expect("Cannot file for this reptag!");
+
+
+
+                    }else{
+                        
+                        _replace_with = _rrv.to_string();
+
+
+                    }
                     _raw = _raw.replace(i.get(0).unwrap().as_str(), &_replace_with);
                 }
             }
 
-            println!("==>\n{}\n<==", _raw);
+            //println!("==>\n{}\n<==", _raw);
+            let out_index_path = &self.output_dir.join("index.gmi");
+            let mut index_file_dsc = File::create(out_index_path).expect("Cannot create index file in output directory");
+            index_file_dsc.write(_raw.as_bytes()).expect("Failed to save index file in output directory");
 
-            //let mut posts = self.get_posts();
-            //let x = Regex::new(r"\[\[\[(\s*)(_posts_)(\s*)\]\]\]").unwrap();
-            //let mut y = x.replace(&index_raw, &self.get_post_list_markup(&mut posts)).to_string();
-            //let mut y = index_raw.replace("[[[_posts_]]]", &self.get_post_list_markup(&mut posts));
-            //for item in _target_keys {
-            //    if self.reptags.contains_key(&item) {
-            //        let _rplc_with_fname = self.reptags.get(&item).unwrap();
-            //        let _raw_file =
-            //            fs::read_to_string(format!("myjam/polu/templates/{}", _rplc_with_fname))
-            //                .expect("not found");
-            //        y = y.replace(format!("[[[{}]]]", item).as_str(), &_raw_file);
-            //    }
-            //}
-            //println!("{}", &y);
         } else {
-            println!("[Err] => Can not find the index file <index.gmi> at the templates folder of your theme!");
+            println!("[Err] => Can not find the entry file {} at the templates folder of your theme!" , self.index_entry.file_name().unwrap().to_str().unwrap());
             no_error = false;
         }
-
-        //self.get_post_title(posts[0].path.to_path_buf());
-        //println!("{:?}" , posts);
-        //println!("{}" , self.get_post_list_markup(&mut posts));
 
         no_error
     }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod jam;
 use jam::*;
 use std::collections::HashMap;
 use std::fs;
+use std::path::Path;
 use std::{path::PathBuf, process::exit};
 use yaml_rust::{Yaml, YamlLoader};
 
@@ -28,20 +29,41 @@ impl MakeJam {
     }
 
     pub fn rocknroll(&self) {
-        let content_dir_str = if !self.config["content_dir"].is_badvalue() {
-            self.config["content_dir"]
+        // ===========================================================
+        // Main Site Configs
+        // ===========================================================
+
+        let content_dir_str = if !self.config["site_config"]["content_dir"].is_badvalue() {
+            self.config["site_config"]["content_dir"]
                 .as_str()
                 .expect("Error in your jam.yaml file -> invalid 'theme_dir'")
         } else {
             panic!("Invalid 'content dir'")
         };
-        let template_dir_str = if !self.config["template_dir"].is_badvalue() {
-            self.config["template_dir"]
+        let template_dir_str = if !self.config["site_config"]["template_dir"].is_badvalue() {
+            self.config["site_config"]["template_dir"]
                 .as_str()
-                .expect("Error in your jam.yaml file -> invalid 'theme_dir'")
+                .expect("Error in your jam.yaml file -> invalid 'template_dir'")
         } else {
             panic!("Invalid 'theme_dir'")
         };
+
+        let output_dir_str = if !self.config["site_config"]["output_dir"].is_badvalue() {
+            self.config["site_config"]["output_dir"]
+                .as_str()
+                .expect("Error in your jam.yaml file -> invalid 'output_dir'")
+        } else {
+            panic!("Invalid 'theme_dir")
+        };
+
+        let index_entry_str = if !self.config["site_config"]["entry"].is_badvalue() {
+            self.config["site_config"]["entry"]
+                .as_str()
+                .expect("Error in your jam.yaml -> Invalid 'entry' point")
+        } else {
+            panic!("Invalid 'entry' point")
+        };
+
         let slug_path = if !self.config["site_config"]["slug_format"].is_badvalue() {
             self.config["site_config"]["slug_format"]
                 .as_str()
@@ -67,6 +89,7 @@ impl MakeJam {
 
         let content_dir: PathBuf = self.working_dir.join(content_dir_str);
         let template_dir: PathBuf = self.working_dir.join(template_dir_str);
+        let output_dir: PathBuf = self.working_dir.join(output_dir_str);
 
         if !template_dir.exists() {
             eprintln!(
@@ -82,10 +105,60 @@ impl MakeJam {
             );
             exit(1);
         }
+        if !output_dir.exists() {
+            eprintln!(
+                "Output directory at {} not found\nCreating it for you!",
+                output_dir.display()
+            );
+            fs::create_dir(&output_dir).unwrap();
+
+            //exit(1);
+        }
+
+        let index_entry: PathBuf = self
+            .working_dir
+            .join(&template_dir_str)
+            .join(index_entry_str);
+        if !index_entry.exists() {
+            eprintln!(
+                "Entry point file at {} not found\nBye!",
+                index_entry.display()
+            );
+            exit(1);
+        }
+
+        // ======================= End of Main Site Configs =========================
+
+        // =====================================================================
+        // Posts Configs
+        // =====================================================================
+        //
+
+        let posts_entry_str = if !self.config["post_config"]["entry"].is_badvalue() {
+            self.config["post_config"]["entry"]
+                .as_str()
+                .expect("Error in your jam.yaml => invalid post template entry file!")
+        } else {
+            eprintln!("Invalid post template entry file!");
+            panic!();
+        };
+
+        let posts_entry = self.working_dir.join(&template_dir_str).join(posts_entry_str);
+
+        if !posts_entry.exists() {
+            eprintln!(
+                "Posts entry file at {} not found\nBye!",
+                posts_entry.display()
+            );
+            exit(1);
+        }
 
         let mut s_builder = builder::structure::StructureBuilder::config(
             content_dir,
             template_dir,
+            output_dir,
+            index_entry,
+            posts_entry,
             slug_path,
             reptags,
         );
